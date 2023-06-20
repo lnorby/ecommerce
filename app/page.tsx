7cb7ab1cbf057d079f5044903fe5b94c7b99b64c
László Norbert <info@laszlonorbert.hu>
2023-06-20 13:17:51 +0200

    Initial commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,32 @@
-import Image from 'next/image'
+interface Page {
+   slug: string;
+   title: {
+      rendered: string;
+   };
+}
 
-export default function Home() {
-  return (
-    <main className="">
-      Nyitóoldal
-    </main>
-  )
+async function loadPages(): Promise<Page[]> {
+   const response = await fetch('https://wp.laszlonorbert.hu/wp-json/wp/v2/pages', {
+      next: {
+         revalidate: 10,
+      },
+   });
+
+   if (!response.ok) {
+      throw new Error('Failed to fetch data');
+   }
+
+   return response.json();
+}
+
+export default async function HomePage() {
+   const pages = await loadPages();
+
+   return (
+      <main className="">
+         {pages.map((page) => (
+            <div key={page.slug}>{page.title.rendered}</div>
+         ))}
+      </main>
+   );
 }
